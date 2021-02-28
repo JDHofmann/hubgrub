47e8e22bf89be72f330e33b0aeb37fa4f42ce7f7
jdhofmann <justindhofmann@gmail.com>
2021-02-28 16:59:24 -0500

    it is now php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $_POST["Insert Your Message"]. "From: jane@janedoe.com");
     <title>HubGrub</title>
 </head>
 <body>
-    <form method="post" action="submit.php">
+    <form method="post" action="index.php">
         <textarea name="message"></textarea>
         <input type="submit">
         </form>
